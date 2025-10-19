40bdacb367987f73539c94841c09dd73006e1ec8
Claudiu <84350723+claudiu28@users.noreply.github.com>
2025-10-19 21:35:29 +0300

    jwt secret

--- server/const/constant.js
+++ server/const/constant.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const SERVER = process.env.SERVER;
 const HOSTNAME = process.env.HOSTNAME;
 const PORT = process.env.PORT;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = {
     SERVER,
     HOSTNAME,
     PORT,
+    JWT_SECRET
 }
